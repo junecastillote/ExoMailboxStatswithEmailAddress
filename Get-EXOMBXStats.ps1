ee5a19fb523418c2c65a514738c4da20bf9985e4
June Castillote <june.castillote@gmail.com>
2025-10-07 08:33:48 +0800

    Add files via upload

---
+++ Get-EXOMBXStats.ps1
@@ -0,0 +1,52 @@
+[CmdletBinding(DefaultParameterSetName = 'MailboxId')]
+param (
+    [Parameter(Mandatory, ParameterSetName = 'MailboxId')]
+    [string]
+    $MailboxId,
+
+    [Parameter(Mandatory, ParameterSetName = 'All')]
+    [switch]
+    $All
+)
+
+if ($PSVersionTable.PSEdition -eq 'Core') {
+    $PSStyle.Progress.View = 'Classic'
+}
+
+$ProgressPreference = 'Continue'
+
+if ($PSCmdlet.ParameterSetName -eq 'MailboxId') {
+    "Getting mailbox ($($MailboxId))" | Out-Default
+    $mailbox = Get-Mailbox -Identity $MailboxId
+}
+if ($PSCmdlet.ParameterSetName -eq 'All') {
+    "Getting all mailbox..." | Out-Default
+    $mailbox = Get-Mailbox -ResultSize Unlimited
+}
+
+if (!$mailbox) {
+    # If no mailbox was retrieve for whatever reason.. terminate the script.
+    return $null
+}
+
+$total = $mailbox.Count
+$counter = 0
+$pctComplete = 0
+
+"Mailbox count: $($total)" | Out-Default
+
+$result = @()
+"Getting mailbox statistics..." | Out-Default
+$mailbox | ForEach-Object {
+    $currentMailbox = $_
+    $counter++
+    $pctComplete = ($counter / $total) * 100
+    Write-Progress -Activity "Getting mailbox statistics... ($([math]::Round($pctComplete,2))%)" -Status "[$($counter) / $($total)] [$($currentMailbox.DisplayName)]"
+
+    $stats = Get-MailboxStatistics -Identity $currentMailbox.ExchangeGuid
+    $stats | Add-Member -MemberType NoteProperty -Name PrimarySmtpAddress -Value $currentMailbox.PrimarySmtpAddress
+
+    $result += $stats
+}
+
+$result
